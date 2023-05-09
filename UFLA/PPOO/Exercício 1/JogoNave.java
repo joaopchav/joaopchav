f47d06fbf4b576868b16eea381afc27729626745
João Paulo Chaves <106988553+joaopchav@users.noreply.github.com>
2023-05-09 17:51:17 -0300

    Add files via upload

---
+++ UFLA/PPOO/Exercício 1/JogoNave.java
@@ -0,0 +1,18 @@
+
+/**
+ * Classe criada para facilitar a execucao do Jogo
+ * 
+ * @author Julio César Alves
+ * @version 2022-05-31
+ */
+public class JogoNave
+{
+    public static void main(String[] args)
+    {
+		Cenario cenario;
+		
+        cenario = new Cenario();
+        
+        cenario.executarLoopDeJogo();
+    }
+}
